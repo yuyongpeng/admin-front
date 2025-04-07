a0beef05ce121ca3e97b879b4aabad32e584571d
yuyongpeng <yuyongpeng@data-assets-finder.com>
2025-04-07 22:23:15 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,35 +5,35 @@ import createVitePlugins from './vite/plugins'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd())
-  const { VITE_APP_ENV } = env
+  const { VITE_APP_ENV, VITE_PORT, VITE_APP_BASE_API, VITE_APP_BASE_URL } = env;
   return {
     // 部署生产环境和开发环境下的URL。
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
     // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
-    base: VITE_APP_ENV === "production" ? "/" : "/",
-    plugins: createVitePlugins(env, command === "build"),
+    base: VITE_APP_ENV === 'production' ? '/' : '/',
+    plugins: createVitePlugins(env, command === 'build'),
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
       alias: {
         // 设置路径
-        "~": path.resolve(__dirname, "./"),
+        '~': path.resolve(__dirname, './'),
         // 设置别名
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
       // https://cn.vitejs.dev/config/#resolve-extensions
-      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     },
     // // vite 相关配置
     server: {
-      port: 80,
+      port: VITE_PORT,
       host: true,
       open: true,
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
-        "/dev": {
-          target: "http://localhost:3000/v1",
+        '/dev': {
+          target: VITE_APP_BASE_URL,
           changeOrigin: true,
-          rewrite: (p) => p.replace(/^\/dev/, ""),
+          rewrite: (p) => p.replace(/^\/dev/, ''),
         },
       },
     },
@@ -42,10 +42,10 @@ export default defineConfig(({ mode, command }) => {
       // 在scss文件添加代码，测试没有成功
       preprocessorOptions: {
         scss: {
-          // api: "modern-compiler" | "modern" | "legacy" 
-          // (如果安装了sass-embedded，默认为"modern-compiler"，否则为 "modern"). 
+          // api: "modern-compiler" | "modern" | "legacy"
+          // (如果安装了sass-embedded，默认为"modern-compiler"，否则为 "modern").
           // 为获得最佳性能，建议使用 api: "modern-compiler" 和 sass-embedded 软件包。"legacy" API 已过时，将在 Vite 7 中移除。
-          api: "modern-compiler", // 自动将 variables.scss 文件引入到所有的 SCSS 文件中。 最好只放变量的scss
+          api: 'modern-compiler', // 自动将 variables.scss 文件引入到所有的 SCSS 文件中。 最好只放变量的scss
           // additionalData: '@use "@/assets/styles/variables.module.scss" as *; ',     // 只要添加了，就报错？？？？？？
           javascriptEnabled: true,
         },
@@ -53,10 +53,10 @@ export default defineConfig(({ mode, command }) => {
       postcss: {
         plugins: [
           {
-            postcssPlugin: "internal:charset-removal",
+            postcssPlugin: 'internal:charset-removal',
             AtRule: {
               charset: (atRule) => {
-                if (atRule.name === "charset") {
+                if (atRule.name === 'charset') {
                   atRule.remove();
                 }
               },
