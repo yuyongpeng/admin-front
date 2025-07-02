5103a6392f53cf5493bb19de071347eb171aaaae
yuyongpeng <yuyongpeng@data-assets-finder.com>
2025-07-02 23:25:46 +0800

    修改一些标识

--- src/views/login.vue
+++ src/views/login.vue
@@ -4,7 +4,7 @@
       <div class="cover-slate"></div>
       <div class="login">
         <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
-          <h3 class="title">方寸记忆后台管理系统</h3>
+          <h3 class="title">方寸记忆 - 后台管理系统</h3>
           <el-form-item prop="username">
             <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" placeholder="账号">
               <template #prefix>
